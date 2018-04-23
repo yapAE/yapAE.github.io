ec6e2e4f11caf6585f7dc47890fa41ecb522bd8b
baidong <baidong@baobeigezi.com>
2018-04-23 14:15:57 +0800

    add

--- push.sh
+++ push.sh
@@ -11,11 +11,11 @@ mv .deploy_git/.git/ ./public/
 
 cd ./public
 
-git config user.name "38139223"
-git config user.email "baidong_6265@qq.com"
+git config user.name "yapAE"
+git config user.email "yapi.ae@zhengheforum.org"
 
 # add commit timestamp
 git add .
 git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
 
-git push --force --quiet "https://${travisToken}@${GH_REF}" master:master
+git push --force --quiet "https://${Token}@${GH_REF}" master:master
